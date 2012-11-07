64ee1b80b34d53b52bf8a15dc69b6697f3c8c789
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2012-11-07 05:01:57 +0000

    [feature] endianness option. default big endian. To show differences.

--- eyeglass.py
+++ eyeglass.py
@@ -9,10 +9,11 @@ class EyePrescription:
 
 	ISODATETIMELEN = 19  #LENGTH OF DATETIME STRING
 
-	def __init__(self):
+	def __init__(self, bigendian=True):
 	
 		self.__eofbof__()
 		self.__date__()
+		self.__endian__(bigendian)
 		
 		#right: oculus dexter (OD)
 		#left:  oculus sinister (OS)
@@ -24,7 +25,7 @@ class EyePrescription:
 		self.prism = {'right': 0, 'left': 0}		# Correction required to help muscles of eyes coordinate (usually blank: float?)
 		self.base = {'right': 0, 'left': 0}		# Direction in which prism is placed (usually blank: float?)
 		
-		self.distance_acuity = {'right': 0, 'left': 0}	#6 divided by ...60 36 24 18 12 9 6 5
+		self.distance_acuity = {'right': 0, 'left': 0}	#6 divided by...60 36 24 18 12 9 6 5
 		self.near_acuity = {'right': 0, 'left': 0}	#N...24 18 14 12 10 8 6 5
 		
 		self.purpose = ''		#purpose of glasses #140 characters
@@ -41,47 +42,61 @@ class EyePrescription:
 		datetime.time(microsecond = 0)
 		d = datetime.datetime.now()
 		self.datetime = d.isoformat('T')[:self.ISODATETIMELEN]		#2012-11-07T00:44:41(minus ms - .000000)
-		
+	
+	def __endian__(self, bigendian):
+		self.bigendian = bigendian
+		if bigendian is not True:
+			self.float = '<f'
+			self.bool = '<?'
+			self.byte = '<B'
+			self.int =  '<i'
+		else:
+			self.float = '>f'
+			self.bool = '>?'
+			self.byte = '>B'
+			self.int =  '>i'
+	
 	def __save__(self, file):
 		file.write(struct.pack('14s', self.magic))
-		file.write(struct.pack('>H', self.version))	#unsigned short
+		file.write(struct.pack(self.byte, self.version))	#unsigned char
+		file.write(struct.pack(self.bool, self.bigendian))	#bool
 		file.write(self.datetime)
 		
 		#padding
 		file.write(struct.pack('>88s', self.padding))
 		
 		#sphere
-		file.write(struct.pack('>f', self.sphere['right']))
-		file.write(struct.pack('>f', self.sphere['left']))
+		file.write(struct.pack(self.float, self.sphere['right']))
+		file.write(struct.pack(self.float, self.sphere['left']))
 		
 		#cylinder
-		file.write(struct.pack('>f', self.cylinder['right']))
-		file.write(struct.pack('>f', self.cylinder['left']))
+		file.write(struct.pack(self.float, self.cylinder['right']))
+		file.write(struct.pack(self.float, self.cylinder['left']))
 		
 		#axis
-		file.write(struct.pack('>i', self.axis['right']))
-		file.write(struct.pack('>i', self.axis['left']))
+		file.write(struct.pack(self.int, self.axis['right']))
+		file.write(struct.pack(self.int, self.axis['left']))
 		
 		#prism
-		file.write(struct.pack('>f', self.prism['right']))
-		file.write(struct.pack('>f', self.prism['left']))
+		file.write(struct.pack(self.float, self.prism['right']))
+		file.write(struct.pack(self.float, self.prism['left']))
 		
 		#base
-		file.write(struct.pack('>f', self.base['right']))
-		file.write(struct.pack('>f', self.base['left']))
+		file.write(struct.pack(self.float, self.base['right']))
+		file.write(struct.pack(self.float, self.base['left']))
 
 		#distance acuity
-		file.write(struct.pack('>f', self.distance_acuity['right']))
-		file.write(struct.pack('>f', self.distance_acuity['left']))
+		file.write(struct.pack(self.float, self.distance_acuity['right']))
+		file.write(struct.pack(self.float, self.distance_acuity['left']))
 		
 		#near acuity
-		file.write(struct.pack('>f', self.near_acuity['right']))
-		file.write(struct.pack('>f', self.near_acuity['left']))
+		file.write(struct.pack(self.float, self.near_acuity['right']))
+		file.write(struct.pack(self.float, self.near_acuity['left']))
 
 		#addendum
 		file.write(struct.pack('140s', self.purpose)) 				#140characters
 		file.write(struct.pack('255s', self.observations))			#255characters
-		file.write(struct.pack('>f', self.next))
+		file.write(struct.pack(self.float, self.next))
 
 		#eof
 		file.write(struct.pack('4s', self.eof))
